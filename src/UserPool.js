d3d2723e0a9ea92fff23800d7a918160c7767141
EC2 Default User <ec2-user@ip-172-31-94-132.ec2.internal>
2021-04-14 23:23:23 +0000

    added basic buttons and input field

---
+++ src/UserPool.js
@@ -0,0 +1,41 @@
+import React, { useState, useRef, useEffect } from "react";
+
+
+export default function UserPool(){
+    const [isClicked, setClicked] = useState(false);
+    const inputRef = useRef(null);
+    const [isConfirmed, setConfirmed] = useState(false);
+
+
+
+    function onPoolButton(){
+        setClicked((prevShown) => !prevShown);
+    }
+    
+    function onConfirmButton(){
+        setConfirmed((prevShown) => !prevShown);
+    }
+    
+    
+
+
+return (
+    <div>
+        {isClicked === true ? (
+        <div>
+            Please enter a name for your pool<input ref={inputRef} type = "text" />
+            <button type="button" onClick={() => onConfirmButton()}> 
+            Confirm
+            </button>
+        </div>
+        ):(
+        <div>
+            <button type="button" onClick={() => onPoolButton()}> 
+            New Pool 
+            </button>
+        </div>
+        )}
+    </div>
+    );
+
+}
