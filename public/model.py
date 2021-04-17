a47eb562fa290288454b4fc8a2fe3ce53d691c1b
Dominik Klimczak <ec2-user@ip-172-31-55-80.ec2.internal>
2021-04-17 19:56:55 +0000

    Database relations

--- public/model.py
+++ public/model.py
@@ -7,38 +7,38 @@ class Images(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     image_name = DB.Column(DB.String(80), unique=True, nullable=False)
     image_location = DB.Column(DB.String(80), unique=True, nullable=False)
+    tags=DB.relationship('Tags',secondary=tags, lazy='subquery', backref=DB.backref('image', lazy=True))
 
     def __repr__(self):
-        return '<Images %r>' % self.image_name
+        return '<Images %r>' % (self.image_name)
         
 class Users(DB.Model):
-    username=DB.Column(DB.Integer, primary_key=True)
+    username=DB.Column(DB.String(80), primary_key=True)
+    password=DB.Column(DB.String(80))
+    pools=DB.relationship('Pools',backref='user',lazy=True)
     
     def __repr__(self):
-        return '<Users %r>' % self.username
+        return '<Users %r>' % (self.username)
         
 class Pools(DB.Model):
+    username=DB.Column(DB.String(80), DB.ForeignKey(Users.username), primary_key=True)
     pool_name=DB.Column(DB.String(80), primary_key=True)
+    images=DB.relationship('Image',secondary=poolimage, lazy='subquery', backref=DB.backref('pool', lazy=True))
     
     def __repr__(self):
-        return '<Pools %r>' % self.pool_name
+        return '<Pools %r>' % (self.pool_name)
         
-class UserPool(DB.Model):
-    username=DB.Column(DB.Integer)
-    pool_name=DB.Column(DB.String(80))
-    
-    def __repr__(self):
-        return '<UserPool %r>' % self.username
-        
-class PoolImage(DB.Model):
-    pool_name=DB.Column(DB.String(80))
-    id = DB.Column(DB.Integer)
-    
-    def __repr__(self):
-        return '<PoolImage %r>' % self.username
-        
-class ImageTags(DB.Model):
-    id = DB.Column(DB.Integer)
+class Tags(DB.Model):
     tag = DB.Column(DB.String(80), primary_key=True)
     def __repr__(self):
-        return '<ImageTags %r>' % self.username
+        return '<Tags %r>' % (self.tag)
+        
+tags = DB.Table('tags',
+DB.Column('image_id',DB.Integer, DB.ForeignKey(Images.id),primary_key=True),
+DB.Column('tag',DB.String(80), DB.ForeignKey(Tags.tag),primary_key=True)
+)
+
+poolimage = DB.Table('Pool_Image',
+DB.Column('pool',DB.String(80), DB.ForeignKey(Pools.pool_name),primary_key=True),
+DB.Column('image_id',DB.Integer, DB.ForeignKey(Images.id),primary_key=True)
+)
