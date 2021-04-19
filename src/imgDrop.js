07c9ca92511193c4b6bed9724317727312c3f8a6
EC2 Default User <ec2-user@ip-172-31-16-210.ec2.internal>
2021-04-19 19:29:30 +0000

    Added a react component to allow users to drag files from their local browser to the page to upload them

---
+++ src/imgDrop.js
@@ -0,0 +1,75 @@
+import React, {useState} from 'react';
+
+function ImgDrop({
+    poolID,
+    socket,
+    addPhotos,
+}) {
+    const [inZone, setInZone] = useState(false);
+    const [fileList, setFileList] = useState([]);
+    
+    function uploadFile(file) {
+        const formData = new FormData()
+        console.log(file)
+        formData.append('myFile', file)
+      
+        fetch('/saveImage', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+            console.error(error)
+        })
+      
+        alert("Upload successful.");
+    }
+    
+    function handleDragEnter(e) {
+        e.preventDefault();
+        setInZone(true);
+        e.stopPropagation();
+    }
+    function handleDragLeave(e) {
+        e.preventDefault();
+        setInZone(false);
+        e.stopPropagation();
+    }
+    function handleDragOver(e) {
+        e.preventDefault();
+        setInZone(true);
+        e.stopPropagation();
+    }
+    function handleDrop(e) {
+        e.preventDefault();
+        e.stopPropagation();
+        if (inZone) {
+            let files = [...e.dataTransfer.files];
+            
+            if (files && files.length > 0){
+                const existingFiles = fileList.map(f => f.name);
+                files = files.filter(f => !existingFiles.includes(f.name));
+                
+                setFileList(prevFiles => prevFiles.concat(files));
+                
+                files.forEach(file => uploadFile(file));
+            }
+        }
+    }
+    
+    return (
+        <div className={'imgDrop'}
+            onDragEnter={e => handleDragEnter(e)}
+            onDragLeave={e => handleDragLeave(e)}
+            onDragOver={e => handleDragOver(e)}
+            onDrop={e => handleDrop(e)}
+        >
+            <p>Drag Image Files Here</p>
+        </div>
+    )
+}
+
+export default ImgDrop;
