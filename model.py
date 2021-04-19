53763076e320e54c3024373142190d14900134b5
Malachi Dube <ec2-user@ip-172-31-16-210.ec2.internal>
2021-04-19 23:34:20 +0000

    Removed explicit relations from database models, renamed classes to be singular

--- model.py
+++ model.py
@@ -6,15 +6,9 @@ from app import db
 #db.Column('image_id',db.Integer, db.ForeignKey('Images.id'), primary_key=True),
 #db.Column('tag',db.String(80), db.ForeignKey('Tags.tag'), primary_key=True)
 #)
-
-poolimage = db.Table('Pool_Image',
-db.Column('pool_name',db.String(80), db.ForeignKey('Pools.pool_name'), primary_key=True),
-db.Column('image_id',db.Integer, db.ForeignKey('Images.id'), primary_key=True)
-)
-
-class Images(db.Model):
+class Image(db.Model):
     __tablename__ = 'Images'
-    id = db.Column(db.Integer, primary_key=True)
+    image_id = db.Column(db.Integer, primary_key=True)
     image_name = db.Column(db.String(80), unique=True, nullable=False)
     image_location = db.Column(db.String(80), unique=True, nullable=False)
 #    tags=db.relationship('Tags',secondary=imagetags, lazy='subquery', backref=db.backref('image', lazy=True))
@@ -22,23 +16,29 @@ class Images(db.Model):
     def __repr__(self):
         return '<Images %r>' % (self.image_name)
         
-class Users(db.Model):
+class User(db.Model):
     __tablename__ = 'Users'
     username=db.Column(db.String(80), primary_key=True)
     password=db.Column(db.String(80))
-    pools=db.relationship('Pools', backref='user', lazy=True)
     
     def __repr__(self):
         return '<Users %r>' % (self.username)
         
-class Pools(db.Model):
+class Pool(db.Model):
     __tablename__ = 'Pools'
     pool_name=db.Column(db.String(80), primary_key=True)
-    username=db.Column(db.String(80), db.ForeignKey('Users.username'), nullable=False)
-    images=db.relationship('Image',secondary=poolimage, lazy='subquery', backref=db.backref('pool', lazy=True))
+    username=db.Column(db.String(80),  nullable=False)
     
     def __repr__(self):
         return '<Pools %r>' % (self.pool_name)
+        
+class PoolItem(db.Model):
+    __tablename__ = 'PoolItems'
+    pool_name = db.Column(db.String(80), primary_key=True)
+    image_id = db.Column(db.Integer, primary_key=True)
+    
+    def __repr__(self):
+        return '<PoolImage %r>' % (self.pool_name)
         
 #class Tags(db.Model):
 #    __tablename__ = 'Tags'
