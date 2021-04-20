c5ffb6b1a5b46270b0a32212b0238c903b6b518e
mithun <ec2-user@ip-172-31-94-132.ec2.internal>
2021-04-20 02:45:36 +0000

    connected user pool function to the flow

---
+++ src/Upload.js
@@ -0,0 +1,44 @@
+import './App.css';
+import React, { useState, useRef, useEffect} from 'react';
+import 'react-dropdown/style.css';
+import io from 'socket.io-client';
+//import axios from 'axios';
+
+
+function Upload() {
+  
+const changeHandler = (event) => {
+  const files = event.target.files
+  const formData = new FormData()
+  console.log(files[0])
+  formData.append('myFile', files[0])
+  
+  fetch('/saveImage', {
+    method: 'POST',
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    console.log(data)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+  
+  alert("Upload successful.");
+
+  // axios.post("/saveImage", formData)
+  //   .then(res => console.log(res))
+  //   .catch(err => console.warn(err));
+};
+
+  
+  return (
+    <div className="App">
+       <input type="file" name="file" onChange={changeHandler} />
+    </div>
+    
+  );
+}
+
+export default Upload;
