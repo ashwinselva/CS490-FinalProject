89c963a7c88a14cb998d1a82306fcb09a9f07c91
Anand Rao <ec2-user@ip-172-31-2-243.ec2.internal>
2021-04-21 03:30:46 +0000

    Created view all pools functionality

--- src/ViewPools.js
+++ src/ViewPools.js
@@ -11,23 +11,54 @@ const socket=io()
 
 export function ViewPools() {
   const [allPools, setAllPools] = useState({});
+  const [showGrid, setShowGrid] = useState(false)
+  const [poolToShow, setPoolToShow] = useState([])
   
   function getPoolNames(){
       socket.emit('viewpools', [])
   }
+  
+  function onShowGrid(val){
+    setShowGrid(!showGrid)
+    const newArray = [...val]
+    setPoolToShow(newArray)
+  }
     useEffect(() => {
-    socket.on('reponse', (data) => {
-        const newDict= [...data]
+    socket.on('response', (data) => {
+        const newDict= {}
+        Object.entries(data).map(([key, val]) =>
+          newDict[key] = val
+        )
         setAllPools(newDict)
+        console.log(newDict)
     });
     
-  }, []);
+ }, []);
   
   return (
     <div>
     <button onClick={getPoolNames}>View all pools</button>
     <div>
-
+      {
+      Object.entries(allPools).map(([key, val]) =>
+        
+        <div>
+           <button onClick={() => onShowGrid(val)}>{key}</button>
+        </div>
+        )
+        
+      }
+      
+        <div>
+        {showGrid === true ? (
+              <div>
+              {poolToShow.map(image => (
+              <img src = {image} />
+              ))}
+              </div>
+        ) : null}
+        </div>
+        
      </div>
      </div>
       
