7cede937a61e324feb98d0134f8a5c36f87bbfa4
Malachi Dube <ec2-user@ip-172-31-16-210.ec2.internal>
2021-04-21 03:09:13 +0000

    Added the ability to see all pools a user has created, and select which pool to add images to as well as see the images in those pools

--- src/UserPool.js
+++ src/UserPool.js
@@ -1,39 +1,86 @@
 import React, { useState, useRef, useEffect } from "react";
-import Upload from "./Upload"
-import imgDrop from "./imgDrop"
+import Upload from "./Upload";
+import ImgDrop from "./imgDrop";
+import ImageList from "./ImageList";
 
 export default function UserPool(props){
-    const [isClicked, setClicked] = useState(false);
+    const [viewMode, setView] = useState(false);
     const inputRef = useRef(null);
     const [isConfirmed, setConfirmed] = useState(false);
-    const [poolName, setName] = useState();
+    const [currentPool, setPool] = useState('');
+    const [poolList, setPoolList] = useState([]);
+    
+    const [waiting, setWaiting] = useState(true);
+    const [imageList, setImages] = useState([]);
+    const [initialized, setInit] = useState(false);
     
     const socket = props.socket;
+    
+    function onPoolSelect(poolName){
+        console.log(poolName);
+        setPool(poolName);
+        setConfirmed(true);
+        setWaiting(true);
+        setView(true);
+        socket.emit('fetchImages', {pool:poolName});
+    }
 
     function onPoolButton(){
-        setClicked((prevShown) => !prevShown);
+        setView((prevShown) => !prevShown);
     }
     
     function onConfirmButton(){
-        setConfirmed((prevShown) => !prevShown);
+        setConfirmed(true);
         const userText = inputRef.current.value;
-        setName(userText);
+        setPool(userText);
+        socket.emit('newPool', {pool_name:currentPool, username:props.username});
+        console.log(isConfirmed);
     }
+    
+    function onShowSelection(){
+        setConfirmed(false);
+        setView(false);
+        socket.emit('fetchPools', {username:props.username});
+    }
+    
+    if (!initialized){
+        socket.emit('fetchPools', {username:props.username});
+        setInit(true);
+    }
+    
+    useEffect(() => {
+        socket.on('list pools', (data) => {
+            setPoolList(data.poolList);
+            console.log('got pools');
+        });
+        socket.on('list images', (data) => {
+            setImages(data.imageList);
+            setWaiting(false);
+            data.imageList.map((image) => console.log(image));
+        });
+    }, []);
     
 return (
     <div>
-        {isClicked === true ? (
+        {viewMode === true ? (
         <div>
             {isConfirmed === true ? (
                 <div>
-                    Displaying {poolName}
-                    <Upload poolName={poolName} username={props.username} scocket={socket}/>
-                    <imgDrop poolName={poolName} username={props.username} socket={socket}/>
+                    Displaying {currentPool}
+                    <button type="button" onClick={onShowSelection}>Choose A Different Pool</button>
+                    <Upload poolName={currentPool} username={props.username} socket={socket}/>
+                    <ImgDrop poolName={currentPool} username={props.username} socket={socket}/>
+                    {waiting === true? (
+                        <div>Waiting</div>
+                    ):(
+                        <ImageList imageList={imageList} />
+                    )
+                    }
                 </div>
             ):(
                 <div>
                     Please enter a name for your pool<input ref={inputRef} type = "text" />
-                    <button type="button" onClick={() => onConfirmButton()}> 
+                    <button type="button" onClick={onConfirmButton}> 
                     Confirm
                     </button>
                 </div>
@@ -41,6 +88,13 @@ return (
         </div>
         ):(
         <div>
+            {
+                poolList.map((pool) => (
+                    <button type="button" onClick={() => onPoolSelect(pool)}>
+                    {pool}
+                    </button>
+                ))
+            }
             <button type="button" onClick={() => onPoolButton()}> 
             New Pool 
             </button>
