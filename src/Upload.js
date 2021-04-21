f9f7d4b857c73d43e0d514fba33ef2e78779a734
Anand Rao <ec2-user@ip-172-31-2-243.ec2.internal>
2021-04-21 00:28:23 +0000

    Removed socketio events

--- src/Upload.js
+++ src/Upload.js
@@ -2,24 +2,19 @@ import './App.css';
 import React, { useState, useRef, useEffect} from 'react';
 import 'react-dropdown/style.css';
 import io from 'socket.io-client';
-//import axios from 'axios';
 
 const socket = io();
 
-function Upload({
-    poolName,
-    addPhotos,
-}
-  ) {
+function Upload(props) {
   
 const changeHandler = (event) => {
   const files = event.target.files
   const formData = new FormData()
   console.log(files[0])
   formData.append('myFile', files[0])
-  formData.append('poolName', poolName)
-
-  socket.emit('new_user_pool', [poolName])
+  formData.append('poolName', props.poolName)
+  formData.append('username', props.username)
+  
   
   fetch('/saveImage', {
     method: 'POST',
@@ -34,10 +29,7 @@ const changeHandler = (event) => {
   })
   
   alert("Upload successful.");
-
-  // axios.post("/saveImage", formData)
-  //   .then(res => console.log(res))
-  //   .catch(err => console.warn(err));
+  
 };
 
   
