aecdb5bf752d74463b483a9450a04f6d8d0cfc66
Malachi Dube <ec2-user@ip-172-31-16-210.ec2.internal>
2021-04-22 18:01:48 +0000

    Cleaned up code for login UI and moved it to its own file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ImgDrop from './imgDrop';
 import io from 'socket.io-client';
 import UserPool from './UserPool';
 import ViewPools from './ViewPools';
-
+import LoginDropdown from './LoginDropdown'
 
 
 const socket = io(); // Connects to socket connection
@@ -21,16 +21,14 @@ function App() {
   const [isNewUserClicked,setNewUserClicked]=useState(false);
   const [username, setUsername]=useState('');
 
+  const searchRef = useRef(null);
   
-  const inputRef = useRef(null);
-  const inputRefUser = useRef(null); 
-  const inputRefPassword = useRef(null); 
-  const inputNewUser = useRef(null); 
-  const inputNewUserPassword = useRef(null); 
+  const usernameRef = useRef(null); 
+  const passwordRef = useRef(null); 
 
-  function onClick(){
-    const username=inputRefUser.current.value;
-    const password=inputRefPassword.current.value;
+  function onLoginComplete(){
+    const username=usernameRef.current.value;
+    const password=passwordRef.current.value;
     console.log(username);
     console.log(password);
     socket.emit('login',{user:username,password:password});
@@ -38,9 +36,9 @@ function App() {
     
     }
 
-  function newUser(){
-    const username=inputNewUser.current.value;
-    const password=inputNewUserPassword.current.value;
+  function onNewUserComplete(){
+    const username=usernameRef.current.value;
+    const password=passwordRef.current.value;
     console.log(username);
     console.log(password);
     socket.emit('newUser',{user:username,password:password});
@@ -67,19 +65,9 @@ function App() {
     });
   }
   
-  function changeLoginClick(){
-    console.log("button clicked");
-    setLoginClicked((prevLogin)=> {
-      return !prevLogin;
-    });
-  }
   
-  function changeNewUserClick(){
-    console.log("button clicked");
-    setNewUserClicked((prevLogin)=> {
-      return !prevLogin;
-    });
-  }
+  
+  
   
   useEffect(() => {
     socket.on('loginSuccess', (data) => {
@@ -96,70 +84,13 @@ function App() {
     <h4 style={{float: 'right', display: 'inline'}}>
     {
     (isLogin === false)?(
-    <div>
-    {
-    isNewUserClicked === false?(
-    <div>
-    <button onClick={()=>changeLoginClick()}>Login</button>
-    <div>
-    {
-      isLoginClicked === true?
-      (<div>
-        <br />
-  <label>
-    Login-ID:
-    <input ref={inputRefUser} type="text"/>
-  </label><br />
-  <label>
-  Password
-    <input type="password" ref={inputRefPassword} />
-  </label><br />
-  <input type="submit" value="Submit" onClick={onClick} />
-  <br />
-  </div>
-      ):
-      (null)
-    }
-    </div>
-    </div>
-    ):(null)
-    }
-    
-    <br />
-    
-    
-    {
-    isLoginClicked === false?(
-    <div>
-    <button onClick={()=>changeNewUserClick()}>New User</button>
-    <div>
-    {
-      isNewUserClicked === true?
-      (
-        <div>
-        <br />
-  <label>
-    User-ID:
-    <input ref={inputNewUser} type="text"/>
-  </label><br />
-  <label>
-  Password
-    <input type="password" ref={inputNewUserPassword} />
-  </label><br />
-  <input type="submit" value="Submit" onClick={newUser} />
-  <br />
-</div>
-      ):
-      (null)
-    }
-    </div>
-    </div>
-    ):(null)
-    }
-    
-    
-    <br />
-    </div>
+    <LoginDropdown 
+      onLoginComplete={onLoginComplete} 
+      onNewUserComplete={onNewUserComplete}
+      usernameRef={usernameRef}
+      passwordRef={passwordRef}
+    />
+      
     ):(
     <div>
       <lable>{username}</lable>
@@ -175,7 +106,7 @@ function App() {
       <button style={buttonstyle}>
       <Dropdown style={buttonstyle} options={options} placeholder="Search by" />
       </button>
-      <input ref={inputRef} type="text" />
+      <input ref={searchRef} type="text" />
       <button type="button" onClick={onSearch}>
         Search
       </button>
