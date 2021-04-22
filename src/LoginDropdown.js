9fe0f00702119eb5aae442e09df2ad43d94702be
Malachi Dube <ec2-user@ip-172-31-16-210.ec2.internal>
2021-04-22 18:06:20 +0000

    Fixed issue where buttons would disappear permanently when clicked

--- src/LoginDropdown.js
+++ src/LoginDropdown.js
@@ -5,25 +5,21 @@ function LoginDropdown(props) {
     const [isLoginClicked,setLoginClicked]=useState(false);
     const [isNewUserClicked,setNewUserClicked]=useState(false);
     
-    function changeLoginClick() {
-        console.log("button clicked");
-        setLoginClicked((prevLogin)=> {
-            return !prevLogin;
-        });
+    function onLoginClick() {
+        setLoginClicked(true);
+        setNewUserClicked(false);
     }
     
-    function changeNewUserClick() {
-        console.log("button clicked");
-        setNewUserClicked((prevLogin)=> {
-            return !prevLogin;
-        });
+    function onNewUserClick() {
+        setNewUserClicked(true);
+        setLoginClicked(false);
     }
   
     return (
         <div>
         <div>
-            {isLoginClicked?(null):(<button onClick={() => changeLoginClick()}>Login</button>)}
-            {isNewUserClicked?(null):(<button onClick={() => changeNewUserClick()}>New User</button>)}
+            {isLoginClicked?(null):(<button onClick={() => onLoginClick()}>Login</button>)}
+            {isNewUserClicked?(null):(<button onClick={() => onNewUserClick()}>New User</button>)}
             {isLoginClicked||isNewUserClicked?(
                 <div>
                 <br />
@@ -38,9 +34,9 @@ function LoginDropdown(props) {
                 </label>
                 <br />
                 {isLoginClicked?(
-                    <input type="submit" value="Submit" onClick={props.onLoginComplete} />
+                    <input type="submit" value="Login" onClick={props.onLoginComplete} />
                 ):(
-                    <input type="submit" value="Submit" onClick={props.onNewUserComplete} />
+                    <input type="submit" value="Create Account" onClick={props.onNewUserComplete} />
                 )}
                 <br />
                 </div>
