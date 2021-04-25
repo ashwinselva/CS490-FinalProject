b822937185eed521eb4c37e8a02044ebd933ff30
Malachi Dube <ec2-user@ip-172-31-16-210.ec2.internal>
2021-04-25 16:36:15 +0000

    I'm not sure what happened but it went from saying I had no conflicts to having one but not really

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import io from 'socket.io-client';
 import ContextManager from './ContextManager';
 import ToolBar from './ToolBar';
 import HomeScreen from './HomeScreen';
-import SwitchTest from './SwitchTest';
 
 
 const socket = io(); // Connects to socket connection
 
 
 function App() {
-  const [contentState, setContent]=useState('test.hi');
+  const [contentState, setContent]=useState('home');
   const [username, setUsername] = useState('');
   
   function setPage(page) {
@@ -20,7 +19,6 @@ function App() {
     const pageData = pageValues.length==2?pageValues[1]:null;
     return {
       'home' : (<HomeScreen />),
-      'test' : (<SwitchTest message={pageData}/>),
     } [pageType]
   }
   
