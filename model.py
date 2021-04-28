4513e348497ded1bc609766843116db218790c90
Anand Rao <ec2-user@ip-172-31-2-243.ec2.internal>
2021-04-28 23:13:19 +0000

    Testing more changes to database

--- model.py
+++ model.py
@@ -5,55 +5,61 @@
 #)
 from app import db
 
-def define_image_class(db):
-    class Image(db.Model):
-        __tablename__ = 'Images'
-        image_id = db.Column(db.Integer, primary_key=True)
-        image_name = db.Column(db.String(80), unique=True, nullable=False)
-        image_url = db.Column(db.String(80), unique=True, nullable=False)
+
+class Image(db.Model):
+    __tablename__ = 'Images'
+    image_id = db.Column(db.Integer, primary_key=True)
+    image_name = db.Column(db.String(80), unique=True, nullable=False)
+    image_url = db.Column(db.String(80), unique=True, nullable=False)
     #    tags=db.relationship('Tags',secondary=imagetags, lazy='subquery', backref=db.backref('image', lazy=True))
     
-        def __repr__(self):
-            return '<Images %r>' % (self.image_name)
-        
-    return Image
-
-
-def define_user_class(db):
-    class User(db.Model):
-        __tablename__ = 'Users'
-        username=db.Column(db.String(80), primary_key=True)
-        password=db.Column(db.String(80))
-        
-        def __repr__(self):
-            return '<Users %r>' % (self.username)
-            
-    return User
-
-
-def define_pool_class(db):    
-    class Pool(db.Model):
-        __tablename__ = 'Pools'
-        pool_name=db.Column(db.String(80), primary_key=True)
-        username=db.Column(db.String(80),  nullable=False)
-        
-        def __repr__(self):
-            return '<Pools %r>' % (self.pool_name)
+    def __repr__(self):
+        return '<Images %r>' % (self.image_name)
     
-    return Pool
-
-
-def define_poolitem_class(db):
-    class PoolItem(db.Model):
-        __tablename__ = 'PoolItems'
-        pool_name = db.Column(db.String(80), primary_key=True)
-        image_id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    def __init__(self, image_id, image_name, image_url):
+        self.image_id = image_id
+        self.image_name = image_name
+        self.image_url = image_url
         
-        def __repr__(self):
-            return '<PoolImage %r>' % (self.pool_name)
-    
-    return PoolItem
 
+
+class User(db.Model):
+    __tablename__ = 'Users'
+    username=db.Column(db.String(80), primary_key=True)
+    password=db.Column(db.String(80))
+        
+    def __repr__(self):
+        return '<Users %r>' % (self.username)
+    
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+    
+    
+class Pool(db.Model):
+    __tablename__ = 'Pools'
+    pool_name=db.Column(db.String(80), primary_key=True)
+    username=db.Column(db.String(80),  nullable=False)
+        
+    def __repr__(self):
+        return '<Pools %r>' % (self.pool_name)
+    
+    def __init__(self, pool_name, username):
+        self.pool_name = pool_name
+        self.username = username
+
+
+class PoolItem(db.Model):
+    __tablename__ = 'PoolItems'
+    pool_name = db.Column(db.String(80), primary_key=True)
+    image_id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+        
+    def __repr__(self):
+        return '<PoolImage %r>' % (self.pool_name)
+    
+    def __init__(self, pool_name, image_id):
+        self.pool_name = pool_name
+        self.image_id = image_id
     
 #class Tags(db.Model):
 #    __tablename__ = 'Tags'
