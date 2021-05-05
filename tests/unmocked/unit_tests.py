ddca4ab581297ff9d9b5cd2a5dd5d08fc994edfa
Anand Rao <ec2-user@ip-172-31-2-243.ec2.internal>
2021-05-05 03:34:11 +0000

    Finished unmocked unit tests

--- tests/unmocked/unit_tests.py
+++ tests/unmocked/unit_tests.py
@@ -1,39 +1,28 @@
 import unittest
-import unittest.mock as mock
-from unittest.mock import patch
 import os
 import sys
 
 sys.path.append(os.path.dirname(os.path.abspath('../')))
 import app
-from app import image_URL
+from app import image_url_url
+from app import get_bucket_name
 
 
 test_image = 'unit_test.jpg'
-image_list = [test_image]
 
 class TestMain(unittest.TestCase):
     
     def setUp(self):
         app.GBUCKET = 'cs490-testbucket'
         
-    @patch('app.image_exists')
-    def mocked_image_exists(self, name):
-        global image_list
-        if name in image_list:
-            return True
-        return False
-    
     def testImageURL(self):
-        test_output = app.image_URL(test_image)
+        test_output = app.image_url_url(test_image)
         self.assertEqual(test_output, 'https://storage.googleapis.com/' + app.GBUCKET + '/' + test_image)
         
-    def testFormatImage(self):
-        with patch('app.image_exists'):
-            test_output = app.format_image(test_image)
-            self.assertEqual(test_output, 'unit_test1.jpg')
+    def testGetBucketName(self):
+        test_output = app.get_bucket_name()
+        self.assertEqual(test_output, app.GBUCKET)
         
-    
         
 if __name__ == "__main__":
     unittest.main()
